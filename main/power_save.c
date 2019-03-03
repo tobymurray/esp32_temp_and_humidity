1bee714dd9da33051ecfbf0f25e104fa51352713
Toby Murray <murray.toby+github@gmail.com>
2019-03-02 21:17:45 -0500

    Add MQTT

--- main/power_save.c
+++ main/power_save.c
@@ -14,11 +14,29 @@
  */
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "esp_event_loop.h"
 #include "esp_pm.h"
+#include "esp_system.h"
+#include "esp_event_loop.h"
+
 #include "nvs_flash.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "mqtt_client.h"
 
 /*set the ssid and password via "make menuconfig"*/
 #define DEFAULT_SSID CONFIG_WIFI_SSID
@@ -37,23 +55,72 @@
 #endif /*CONFIG_POWER_SAVE_MODEM*/
 
 static const char *TAG = "power_save";
+static EventGroupHandle_t wifi_event_group;
+const static int CONNECTED_BIT = BIT0;
+
+static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event) {
+	esp_mqtt_client_handle_t client = event->client;
+	int msg_id;
+	switch (event->event_id) {
+	case MQTT_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+		msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+		msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+		ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+		break;
+
+	case MQTT_EVENT_SUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		break;
+	case MQTT_EVENT_UNSUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_PUBLISHED:
+		ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		break;
+	case MQTT_EVENT_DATA:
+		ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+		printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		printf("DATA=%.*s\r\n", event->data_len, event->data);
+		break;
+	case MQTT_EVENT_ERROR:
+		ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+		break;
+	default:
+		ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+		break;
+	}
+	return ESP_OK;
+}
 
 static esp_err_t event_handler(void *ctx, system_event_t *event) {
 	switch (event->event_id) {
 	case SYSTEM_EVENT_STA_START:
 		ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
-		ESP_ERROR_CHECK(esp_wifi_connect())
-		;
+		ESP_ERROR_CHECK(esp_wifi_connect());
 		break;
 	case SYSTEM_EVENT_STA_GOT_IP:
 		ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
-		ESP_LOGI(TAG, "got ip:%s\n",
-				ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+		ESP_LOGI(TAG, "got IP:%s\n", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+		xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
 		break;
 	case SYSTEM_EVENT_STA_DISCONNECTED:
 		ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
-		ESP_ERROR_CHECK(esp_wifi_connect())
-		;
+		ESP_ERROR_CHECK(esp_wifi_connect());
+		xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
 		break;
 	default:
 		break;
@@ -64,26 +131,73 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
 /*init wifi as sta and set power save mode*/
 static void wifi_power_save(void) {
 	tcpip_adapter_init();
+	wifi_event_group = xEventGroupCreate();
 	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT()
-	;
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-	wifi_config_t wifi_config = { .sta = { .ssid = DEFAULT_SSID, .password =
-			DEFAULT_PWD, .listen_interval = DEFAULT_LISTEN_INTERVAL, }, };
+	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+	wifi_config_t wifi_config = {
+			.sta = {
+					.ssid = DEFAULT_SSID,
+					.password =	DEFAULT_PWD,
+					.listen_interval = DEFAULT_LISTEN_INTERVAL,
+			},
+	};
 	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+
+	ESP_LOGI(TAG, "start the Wi-Fi SSID:[%s]", CONFIG_WIFI_SSID);
 	ESP_ERROR_CHECK(esp_wifi_start());
+
+	ESP_LOGI(TAG, "Waiting for wifi");
+	xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
 
 	ESP_LOGI(TAG, "esp_wifi_set_ps().");
 	esp_wifi_set_ps(DEFAULT_PS_MODE);
 }
 
-void app_main() {
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = CONFIG_BROKER_URL,
+        .event_handle = mqtt_event_handler,
+        // .user_context = (void *)your_context
+    };
+
+#if CONFIG_BROKER_URL_FROM_STDIN
+    char line[128];
+
+    if (strcmp(mqtt_cfg.uri, "FROM_STDIN") == 0) {
+        int count = 0;
+        printf("Please enter url of mqtt broker\n");
+        while (count < 128) {
+            int c = fgetc(stdin);
+            if (c == '\n') {
+                line[count] = '\0';
+                break;
+            } else if (c > 0 && c < 127) {
+                line[count] = c;
+                ++count;
+            }
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+        mqtt_cfg.uri = line;
+        printf("Broker url: %s\n", line);
+    } else {
+        ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
+        abort();
+    }
+#endif /* CONFIG_BROKER_URL_FROM_STDIN */
+
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_start(client);
+}
+
+void start_up_stuff() {
 	// Initialize NVS
 	esp_err_t storage_init = nvs_flash_init();
-	if (storage_init == ESP_ERR_NVS_NO_FREE_PAGES
-			|| storage_init == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+	if (storage_init == ESP_ERR_NVS_NO_FREE_PAGES || storage_init == ESP_ERR_NVS_NEW_VERSION_FOUND) {
 		ESP_ERROR_CHECK(nvs_flash_erase());
 		storage_init = nvs_flash_init();
 	}
@@ -93,17 +207,32 @@ void app_main() {
 	// Configure dynamic frequency scaling:
 	// maximum and minimum frequencies are set in sdkconfig,
 	// automatic light sleep is enabled if tickless idle support is enabled.
-	esp_pm_config_esp32_t pm_config = { .max_freq_mhz =
-			CONFIG_EXAMPLE_MAX_CPU_FREQ_MHZ, .min_freq_mhz =
-			CONFIG_EXAMPLE_MIN_CPU_FREQ_MHZ,
+	esp_pm_config_esp32_t pm_config = {
+			.max_freq_mhz =	CONFIG_EXAMPLE_MAX_CPU_FREQ_MHZ,
+			.min_freq_mhz =	CONFIG_EXAMPLE_MIN_CPU_FREQ_MHZ,
 #if CONFIG_FREERTOS_USE_TICKLESS_IDLE
 			.light_sleep_enable = true
 #endif
 			};
 	ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
 #endif // CONFIG_PM_ENABLE
+}
 
+void app_main() {
+  ESP_LOGI(TAG, "[APP] Startup..");
+	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+	ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+	esp_log_level_set("*", ESP_LOG_INFO);
+	esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+	esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+	start_up_stuff();
 	wifi_power_save();
+	mqtt_app_start();
 
 	ESP_LOGI(TAG, "Everything is all set up.");
 }
